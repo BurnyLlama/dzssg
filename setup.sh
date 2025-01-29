80d2a905cc2ada3061ff3348cb9db67b283bc97d
BurnyLlama <burnyllama@protonmail.com>
2025-01-29 23:33:11 +0100

    Updated setup script to reflect expected folder structure.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
-mkdir -p content/{media,posts}
+#!/bin/env bash
+
+source .env
+
+echo "Creating directories..."
+mkdir -p "$CONTENT_PATH/{fonts,media,posts,scss,views/{components,pages,templates}}"
+
 mkdir out
